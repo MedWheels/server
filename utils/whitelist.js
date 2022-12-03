6fd44a75a0f3dafe8d897a2428c079fb52644001
Rohith G <gangadharrohith6@gmail.com>
2022-12-03 17:04:11 +0530

    origin issues

--- utils/whitelist.js
+++ utils/whitelist.js
@@ -2,8 +2,8 @@ const whitelist = [
 	//testing
 	"http://localhost:3000",
 	//production
-	"https://medwheelsuser.vercel.app/",
-	"https://hospital-lemon.vercel.app/"
+	"https://medwheelsuser.vercel.app",
+	"https://hospital-lemon.vercel.app"
 ]
 
 export default whitelist
